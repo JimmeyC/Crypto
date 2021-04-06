746cd49be530b1a6f71cf75b55aec975b818c79a
poggers1337 <49186603+poggers1337@users.noreply.github.com>
2021-04-06 23:54:30 +1000

    added more idk

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import discord
 import requests
-#defines
+import os
+#from dotenv import load_dotenv
 
+#start up
+
+f = open("data/token.txt", "r")
+TOKEN = f.readlines(1)
+f.close()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord successfully!')
+
+client.run('Token') #replace token with your own token
+
+
+
+
+
+#defines
 
 def get_prices():
     coins = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "LINK", "BNB", "XLM", "WIN" , "HNT" , "BAT"]  #You can Add more later
@@ -19,3 +38,15 @@ def get_prices():
         }
 
     return data
+
+def start(update, context):
+    chat_id = update.effective_chat.id
+    message = ""
+
+    crypto_data = get_prices()
+    for i in crypto_data:
+        coin = crypto_data[i]["coin"]
+        price = crypto_data[i]["price"]
+        change_day = crypto_data[i]["change_day"]
+        change_hour = crypto_data[i]["change_hour"]
+        message += f"Coin: {coin}\nPrice: ${price:,.2f}\nHour Change: {change_hour:.3f}%\nDay Change: {change_day:.3f}%\n\n"
