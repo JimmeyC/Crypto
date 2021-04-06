62260e5f18abf2e0240c8fc27b0679667000c0bc
poggers1337 <49186603+poggers1337@users.noreply.github.com>
2021-04-06 23:35:18 +1000

    cheeze

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import discord
 import requests
+#defines
+
+
+def get_prices():
+    coins = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "LINK", "BNB", "XLM", "WIN" , "HNT" , "BAT"]  #You can Add more later
+
+    crypto_data = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD".format(",".join(coins))).json()["RAW"]
+
+    data = {}
+    for i in crypto_data:
+        data[i] = {
+            "coin": i,
+            "price": crypto_data[i]["USD"]["PRICE"],
+            "change_day": crypto_data[i]["USD"]["CHANGEPCT24HOUR"],
+            "change_hour": crypto_data[i]["USD"]["CHANGEPCTHOUR"]
+        }
+
+    return data
